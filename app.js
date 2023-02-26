4cef040981b1a97e94b198c85a3e6b9f705b5641
jradnan <mohd.yasin085208@gmail.com>
2023-02-26 13:05:39 +0600

    done

--- app.js
+++ app.js
@@ -1,89 +1,89 @@
 const loadMeals = (searchText) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
     `;
 
-    console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showMeal(data.meals))
-        .catch(err => console.log(err))
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showMeal(data.meals))
+    .catch(err => console.log(err))
 }
 
 
 const showMeal = mealData => {
-    const container = document.getElementById("container");
-    console.log(mealData);
-    container.innerHTML = "";
-    mealData.forEach(meal => {
-        console.log(meal);
-        const div = document.createElement("div");
-        
-        div.innerHTML = `
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+
+  mealData.forEach(meal => {
+
+
+    const div = document.createElement("div");
+
+    div.innerHTML = `
         <div class="card px-[24px] lg:card-side bg-base-100 shadow-xl">
         <figure><img src="${meal.strMealThumb}"></figure>
         <div class="card-body">
           <h2 class="card-title">${meal.strMeal}</h2>
-          <p>${meal.strInstructions.slice(0,80)}</p>
+          <p>${meal.strInstructions.slice(0, 80)}</p>
           <!-- The button to open modal -->
-          <label for="my-modal" class="text-[18px] text-[#FFC107] font-[600]">View Details</label>
+          <label for="my-modal" onClick="loadDetails('${meal.idMeal}')"  class="text-[18px] text-[#FFC107] font-[600]">View Details</label>
           
 
-<!-- Put this part before </body> tag -->
-          
-<input type="checkbox" id="my-modal" class="modal-toggle" />
 
-<div class="modal">
-  <div class="modal-box">
-  <figure><img src="${meal.strMealThumb}"></figure>
- 
-    <p class="py-4">${meal.strInstructions}</p>
-    <div class="modal-action">
-      <label for="my-modal" class="btn border-none bg-[red]">Close</label>
-    </div>
-  </div>
-</div>
+         
 
-</div>
-          
-          
 
-       
-       
+        
+     </div>
+
         `;
 
 
-        container.appendChild(div)
-        
-    })
+    container.appendChild(div)
 
-   
+
+  })
+
+
 }
 
 const searchMeal = () => {
-    const inputField = document.getElementById("input-field").value;
-    loadMeals(inputField)
+  const inputField = document.getElementById("input-field").value;
+  loadMeals(inputField)
 
 }
 
+
+const loadDetails = id => {
+
+
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showDetails(data.meals))
+    .catch(err => console.log(err))
+}
+
+const showDetails = data => {
+
+  data.forEach(details => {
+    console.log(details);
+    const image = document.getElementById("img");
+    image.innerHTML = `
+    <img src="${details.strMealThumb}">
+    `
+    const mealDetails = document.getElementById("mealDetails");
+    mealDetails.innerText = details.strInstructions;
+  })
+}
 
 loadMeals('rice');
 
 
 
 
-// <!-- Put this part before </body> tag -->
-          
-//           <input type="checkbox" id="my-modal" class="modal-toggle" />
-          
-//           <div class="modal">
-//             <div class="modal-box">
-//             <figure><img src="${meal.strMealThumb}"></figure>
-           
-//               <p class="py-4">${meal.strInstructions}</p>
-//               <div class="modal-action">
-//                 <label for="my-modal" class="btn border-none bg-[red]">Close</label>
-//               </div>
-//             </div>
-//           </div>
-         
-//         </div>
+
+
+
+
+
